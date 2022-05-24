b588611f20a4b7e667f30655a9e30295f0c0af2b
fob2 <finbarrobren95@gmail.com>
2022-05-24 14:13:50 +0000

    commit 2

--- ingredients/models.py
+++ ingredients/models.py
@@ -3,29 +3,36 @@ from django.db import models
 from datetime import datetime, date
 
 class Ingredients(models.Model):
-    ingredients = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    stock = models.IntegerField(null=True)
+    ingredients = models.CharField(max_length=30, null=False)
+    #price = models.DecimalField(max_digits=5, decimal_places=2)
+    #stock = models.IntegerField(null=True)
 
     def __str__(self):
         return self.ingredients
 
 class Menu_Items(models.Model):
-    menu_item = models.CharField(max_length=30, null=True)
-    price = models.FloatField(null=True)
+    menu_items = models.CharField(max_length=30, null=False)
+    ingredients = models.ManyToManyField(Ingredients, through="Stock_Required")
+    #price = models.FloatField(null=True)
     
     def __str__(self):
         return self.menu_item
 
-class Purchase_History(models.Model):
+class Stock_Required(models.Model):
+    menu_items = models.ForeignKey(Menu_Items, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    ingredients_quantity = models.IntegerField(null=False)
+    
+
+""" class Purchase_History(models.Model):
     menu_item = models.ForeignKey(Menu_Items, null=True, on_delete=models.SET_NULL)
     price = models.ForeignKey(Menu_Items, null=True, on_delete=models.SET_NULL, related_name="meal_price")
     date_purchased = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f"{self.menu_item} {self.date_purchased}"
+        return f"{self.menu_item} {self.date_purchased}" """
 
-#class Recipes(models.Model):
+
     
     
 
